be0a168abf008ebbd70faa80f3cc0460f70c2b08
marcopavon <marco.pavoni19@yahoo.com>
2021-01-17 18:20:21 +0100

    changed text in TXT

--- Time.py
+++ Time.py
@@ -3,5 +3,17 @@ import time
 exp = time.asctime( time.localtime(time.time()))
 def show():
     print(exp)
+    return 2
 
 show()
+
+
+class filler:
+    def __init__(self):
+        self.x = "mr. X"
+        self.y = exp
+        self.z = show()
+        print("class filler in console")
+        
+ob = filler()
+print(ob.x,ob.y,ob.z)
